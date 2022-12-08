4a7513b624041b4c8e2175406db6dd0485c85c95
Hans Shen <116024825+cameragrammar@users.noreply.github.com>
2022-12-08 10:54:33 -0700

    Add files via upload

--- assets/script.js
+++ assets/script.js
@@ -1,96 +1,88 @@
-const dateEl = document.getElementById('date');
-const timeEl = document.getElementById('time');
-const currentWeatherItemsEl = document.getElementById('current-weather-items');
-const timezone = document.getElementById('time-zone');
-const countryEl = document.getElementById('country')
-const weatherForecastEl = document.getElementById('weather-forecast');
-const currentTempEl = document.getElementById('current-temp');
+//call variables
 
+var contentEl = document.querySelector("#content");
+var weatherEl = document.querySelector("#weather-data");
 
-const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
-const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+var APIkey = "d064d50bdad3977fc6ae9d07fb12482e";
 
-const API_KEY ='d064d50bdad3977fc6ae9d07fb12482e';
-
-var cityHistory = [];
-if (localStorage.getItem("cityHistory")) {
-  cityHistory = JSON.parse(localStorage.getItem("cityHistory"));
-  createHistoryList(cityHistory);
-}
-
-
- else {
-  
-        $("#main").removeAttr("style");
-  if (cityHistory[0]) {
-    getCurrentWeather(cityHistory[cityHistory.length -1]);
-    getFiveDayForecast(cityHistory[cityHistory.length-1]);
-  } else {
-    getCurrentWeather("Salt Lake City");
-    getFiveDayForecast("Salt Lake City");
-  }
-}
-
-function createHistoryList(historyArray) {
-  for (var i = 0; i < historyArray.length; i++) {
-    var newA = $("<a>");
-    newA.attr("class", "panel-block");
-    newA.text(historyArray[i]);
-    newA.attr("id", historyArray[i]);
-    $("#cityHistoryContainer").prepend(newA);
-  }
-}
-
-setInterval(() => {
-  const time = new Date();
-  const month = time.getMonth();
-  const date = time.getDate();
-  const day = time.getDay();
-  const hour = time.getHours();
-  const hoursIn12HrFormat = hour >= 13 ? hour %12: hour
-  const minutes = time.getMinutes();
-  const ampm = hour >=12 ? 'PM' : 'AM'
-
-  timeEl.innerHTML = (hoursIn12HrFormat < 10? '0'+hoursIn12HrFormat : hoursIn12HrFormat) + ':' + (minutes < 10? '0'+minutes: minutes)+ ' ' + `<span id="am-pm">${ampm}</span>`
-
-  dateEl.innerHTML = days[day] + ', ' + ' ' + months[month] + ' ' + date
-
-}, 1000);
-
-
-getWeatherData()
-function getWeatherData () {
-    navigator.geolocation.getCurrentPosition((success) => {
-      
-      let {latitude, longitude } = success.coords;
-
-      fetch('https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&units=imperial&appid=${API_Key}').then(res => res.json()).then(data => {
-
-      console.log(data)
-      showWeatherData(data);
-      })
+//get current weather
+function getWeather(city) {
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + APIkey;
+    fetch(apiUrl)
+    .then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                displayWeather(data, city);
+            });
+        } else {
+            alert("Error: " + response.statusText);
+        }
     })
+    .catch(function(error) {
+        alert("Unable to connect to OpenWeather");
+    });
 }
 
-function showWeatherData (data){
-  let {temp, pressure, wind_speed, uvi} = data.current;
+//get 5 day forecast
+function getForecast(city) {
+    var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=" + APIkey;
+    fetch(apiUrl)
+    .then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                displayForecast(data);
+            });
+        } else {
+            alert("Error: " + response.statusText);
+        }
+    })
+    .catch(function(error) {
+        alert("Unable to connect to OpenWeather");
+    }
+    );
+}
 
-  currentWeatherItemsEl.innerHTML = 
-    `<div class="weather-item">
-          <div>Temperature</div>
-          <div>${temp}</div>
-        </div>
-        <div class="weather-item">
-          <div>Pressure</div>
-          <div>${pressure}</div>
-        </div>
-        <div class="weather-item">
-          <div>Wind Speed</div>
-          <div>${wind_speed}</div>
-        </div>
-        <div class="weather-item">
-          <div>UV Index</div>
-          <div>${uvi}</div>
-        </div>`;
+//display current weather
+function displayWeather(data, city) {
+    //clear old content
+    weatherEl.textContent = "";
+    contentEl.textContent = "";
 
+    //create date element
+    var currentDate = document.createElement("span");
+    currentDate.textContent = " (" + moment(data.dt.value).format("MMM D, YYYY") + ") ";
+    contentEl.appendChild(currentDate);
+
+    //create an image element
+    var weatherIcon = document.createElement("img");
+    weatherIcon.setAttribute("src", "https://openweathermap.org/img/wn/" + data.weather[0].icon + ".png");
+    contentEl.appendChild(weatherIcon);
+
+    //create a span element to hold temperature data
+    var temperatureEl = document.createElement("span");
+    temperatureEl.textContent = "Temperature: " + data.main.temp + " °F";
+    temperatureEl.classList = "list-group-item";
+
+    //create a span element to hold humidity data
+    var humidityEl = document.createElement("span");
+    humidityEl.textContent = "Humidity: " + data.main.humidity + "%";
+    humidityEl.classList = "list-group-item";
+
+    //create a span element to hold wind speed data
+    var windSpeedEl = document.createElement("span");
+    windSpeedEl.textContent = "Wind Speed: " + data.wind.speed + " MPH";
+    windSpeedEl.classList = "list-group-item";
+
+    //append to container
+    contentEl.appendChild(temperatureEl);
+
+    //append to container
+    contentEl.appendChild(humidityEl);
+
+    //append to container
+    contentEl.appendChild(windSpeedEl);
+
+    var lat = data.coord.lat;
+    var lon = data.coord.lon;
+    getUvIndex(lat, lon);
 }
